5e3327e74a2984855482e6987fdc841c6dcfd0c7
Britto Baby <brittobaby23@gmail.com>
2025-08-10 22:50:11 +0900

    New Folder

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  count         = var.instance_count
+
+  tags = {
+    Name = "My-T3-Micro-${count.index + 1}"
+  }
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "ami_id" {
+  default = "ami-0df99b3a8349462c6"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "instance_count" {
+  default = 3
+}
